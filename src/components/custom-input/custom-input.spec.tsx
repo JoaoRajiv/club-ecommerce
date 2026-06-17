ac8a6fdaeba736d5bef37ee188c7e8c93e93aef5
JoaoRajiv <olivairarajiv@gmail.com>
2026-06-16 23:57:10 -0300

    test: add unit tests for CustomInput component to validate error handling and user input

--- src/components/custom-input/custom-input.spec.tsx
+++ src/components/custom-input/custom-input.spec.tsx
@@ -1,4 +1,6 @@
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import Colors from "../../theme/theme.colors";
 import CustomInput from "./custom-input.component";
 
@@ -9,5 +11,22 @@ describe("CustomInput", () => {
 		);
 		const input = getByPlaceholderText("O e-mail é obrigatório.");
 		expect(input).toHaveStyle({ border: `2px solid ${Colors.error}` });
+	});
+
+	it("should render without error if hasError is false", () => {
+		const { getByPlaceholderText } = render(
+			<CustomInput placeholder="O e-mail é obrigatório." hasError={false} />,
+		);
+		const input = getByPlaceholderText("O e-mail é obrigatório.");
+		expect(input).toHaveStyle({ border: "none" });
+	});
+
+	it("should change value when user types", async () => {
+		const { getByPlaceholderText, getByDisplayValue } = render(
+			<CustomInput placeholder="O e-mail é obrigatório." hasError={false} />,
+		);
+		const input = getByPlaceholderText("O e-mail é obrigatório.");
+		userEvent.type(input, "test@test.com");
+		getByDisplayValue("test@test.com");
 	});
 });
